823a316086eb1808ba126dcd6518831ecdaecd29
JeremySeq <jaysequeir@gmail.com>
2026-06-27 11:46:08 -0500

    add simple movement (no look control)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     InputAction moveAction;
+    InputAction jumpAction;
+    Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,7 +20,8 @@ public class PlayerMovement : MonoBehaviour
     {
 
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        float jumpValue = jumpAction.ReadValue<float>();
 
-        Debug.Log(moveValue.x + ", " + moveValue.y);
+        rb.AddForce(new Vector3(moveValue.x * 2, jumpValue * 5, moveValue.y * 2));
     }
 }
