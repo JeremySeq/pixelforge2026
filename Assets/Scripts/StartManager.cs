96f5d7da04617de6480aaf4be4fcae0270b79239
mattseq <matthewsequeir@gmail.com>
2026-06-30 12:00:51 -0500

    start scene; start, exit, volume slider

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+  public void PlayGame()
+  {
+      Debug.Log("Play clicked!");
+      int index = SceneManager.GetActiveScene().buildIndex;
+      SceneManager.LoadScene(index + 1);
+  }
+
+  public void QuitGame()
+  {
+      Application.Quit();
+  }
+
+  public void SetVolume(float sliderValue) {
+    AudioListener.volume = sliderValue;
+  }
+}
