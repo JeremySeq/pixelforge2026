6b9e39b190ebf7711aa92141df4c63fde4e388d2
mattseq <matthewsequeir@gmail.com>
2026-06-30 23:29:11 -0500

    controls panel for start scene

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -3,18 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
-  public void PlayGame()
-  {
-      int index = SceneManager.GetActiveScene().buildIndex;
-      SceneManager.LoadScene(index + 1);
-  }
+    public GameObject controlPanel;
 
-  public void QuitGame()
-  {
-      Application.Quit();
-  }
+    void Start()
+    {
+        controlPanel.SetActive(false);
+    }
 
-  public void SetVolume(float sliderValue) {
-    AudioListener.volume = sliderValue;
-  }
+    public void PlayGame()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume(float sliderValue) {
+        AudioListener.volume = sliderValue;
+    }
+
+    public void openControlPanel()
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void closeControlPanel()
+    {
+        controlPanel.SetActive(false);
+    }
 }
